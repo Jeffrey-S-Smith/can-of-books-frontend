9c1d1c3efa33e06baf351325edadc9e729dd955a
Brooke Heck <b19heck@gmail.com>
2022-08-31 15:05:05 -0700

    remove unused variable

--- src/UpdateModal.js
+++ src/UpdateModal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 import Button  from 'react-bootstrap/Button';
